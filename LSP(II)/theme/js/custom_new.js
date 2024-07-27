a8b91c172104b074241476c9cfea57c512d24b75
deaagustina <deaagustinahuang02@gmail.com>
2024-07-28 03:10:08 +0700

    modal & tampilan done

--- LSP(II)/theme/js/custom_new.js
+++ LSP(II)/theme/js/custom_new.js
@@ -10,11 +10,7 @@ function confirmDelete(button) {
       closeOnConfirm: !1,
     },
     function () {
-      swal(
-        "Berhasil !!",
-        "Untuk lanjut ke APL 02, silahkan menunggu admin approve",
-        "success"
-      );
+      swal("Berhasil !!", "Data Berhasil di Hapus", "success");
     }
   );
 }
@@ -41,3 +37,357 @@ function simpan_data() {
     }
   );
 }
+$(".hapus_data_pengajaran").on("click", function () {
+  $(".mata_kuliah").val("");
+  $(".kelas_pengajaran").val("");
+  $(".input_bukti_pengajaran").val("");
+});
+
+// js for delete row data pengajaran
+$("#tambah-riwayat-pengajaran").click(function () {
+  var newRow = `<tr>
+                        <td>
+                            <input type="text" class="form-control mata_kuliah" />
+                        </td>
+                        <td>
+                            <input type="text" class="form-control kelas_pengajaran" />
+                        </td>
+                        <td width="28%">
+                            <input type="file" class="form-control kelas_pengajaran" />
+                        </td>
+                        <td>
+                            <button class="btn btn-danger hapus_data_pengajaran" type="button">Hapus</button>
+                        </td>
+                      </tr>`;
+  $("#riwayat-pengajaran-body").append(newRow);
+});
+
+$(document).on("click", ".hapus_data_pengajaran", function () {
+  $(this).closest("tr").remove();
+});
+
+$(".button_tambah_pengalaman_pelatihan").on("click", function () {
+  var row_pelatihan = `<tr>
+                    <td>
+                      <input type="text" class="form-control" />
+                    </td>
+                    <td>
+                      <input type="text" class="form-control" />
+                    </td>
+                    <td>
+                      <input type="date" class="form-control" />
+                    </td>
+                    <td>
+                      <div class="custom-file">
+                        <input
+                          type="file"
+                          class="custom-file-input"
+                          id="buktiPelatihan1"
+                        />
+                        <label class="custom-file-label" for="buktiPelatihan1"
+                          >Pilih file</label
+                        >
+                      </div>
+                    </td>
+                    <td>
+                      <button class="btn btn-danger button_hapus_pengalaman_pelatihan" type="button">Hapus</button>
+                    </td>
+                  </tr>`;
+  $(".row_tambah_pengalaman_pelatihan").append(row_pelatihan);
+});
+$(document).on("click", ".button_hapus_pengalaman_pelatihan", function () {
+  $(this).closest("tr").remove();
+});
+// $(".button_hapus_pengalaman_pelatihan").on("click", function(){
+//   $(this).closest("tr").remove();
+// })
+
+// riwayat pengajaran
+$(".button_tambah_riwayat_pengajaran").on("click", function () {
+  var row_pelatihan = ` <tr>
+                                                <td>
+                                                    <div class="col-lg-12">
+                                                        <input
+                                                            type="text"
+                                                            class="form-control"
+                                                            id="val-matakuliah1"
+                                                            name="val-matakuliah1"
+                                                        />
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="col-lg-12">
+                                                        <input
+                                                            type="text"
+                                                            class="form-control"
+                                                            id="val-kelas1"
+                                                            name="val-kelas1"
+                                                        />
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                    <div class="button-icon ">
+                                                        <input
+                                                            type="file"
+                                                            class="form-control"
+                                                            id="exampleFormControlFile1"
+                                                            accept="image/*"
+                                                        />
+                                                    </div>
+                                                </td>
+                                                <td>
+                                                  <button class="btn btn-danger button_hapus_riwayat_pengajaran">
+                                                        Hapus
+                                                  </button>
+                                                </td>
+                                            </tr>`;
+  $(".row_riwayat_pengajaran").append(row_pelatihan);
+});
+$(document).on("click", ".button_hapus_riwayat_pengajaran", function () {
+  $(this).closest("tr").remove();
+});
+
+// pengalaman pelatihan
+$(".button_tambah_pengalaman_pelatihan1").on("click", function () {
+  var row_pelatihan = `<tr>
+                              <td>
+                                <div class="col-lg-12">
+                                  <input
+                                    type="text"
+                                    class="form-control"
+                                    id="val-jenispelaksanaan1"
+                                    name="val-pelaksanaan1"
+                                  />
+                                </div>
+                              </td>
+                              <td>
+                                <div class="col-lg-12">
+                                  <input
+                                    type="text"
+                                    class="form-control"
+                                    id="val-pelaksanaan1"
+                                    name="val-pelaksanaan1"
+                                  />
+                                </div>
+                              </td>
+                              <td>
+                                <div class="col-lg-12">
+                                  <input
+                                    type="text"
+                                    class="form-control"
+                                    id="val-tahun"
+                                    name="val-tahun"
+                                  />
+                                </div>
+                              </td>
+                              <td>
+                                <div class="button-icon">
+                                  <input
+                                    type="file"
+                                    class="form-control"
+                                    id="exampleFormControlFile1"
+                                    accept="image/*"
+                                  />
+                                </div>
+                              </td>
+                              <td>
+                                <button class="btn btn-danger button_hapus_pengalaman_pelatihan1">
+                                      Hapus
+                                </button>
+                              </td>
+                            </tr>`;
+  $(".row_pengalaman_pelatihan1").append(row_pelatihan);
+});
+$(document).on("click", ".button_hapus_pengalaman_pelatihan1", function () {
+  $(this).closest("tr").remove();
+});
+$(".simpan_data_pengalaman").on("click", function () {
+  var is_valid = true;
+
+  var input_nama_lengkap_asesor = $(".input_nama_lengkap_asesor").val();
+  var error_input_nama_lengkap_asesor = $(".error_input_nama_lengkap_asesor");
+
+  if (input_nama_lengkap_asesor === "") {
+    error_input_nama_lengkap_asesor.text("masukkan nama lengkap");
+    is_valid = false;
+  } else {
+    error_input_nama_lengkap_asesor.text("");
+  }
+
+  var input_inisial_asesor = $(".input_inisial_asesor").val();
+  var error_input_inisial_asesor = $(".error_input_inisial_asesor");
+
+  if (input_inisial_asesor === "") {
+    error_input_inisial_asesor.text("masukkan inisial");
+    is_valid = false;
+  } else {
+    error_input_inisial_asesor.text("");
+  }
+
+  var input_tempat = $(".input_tempat").val();
+  var error_input_tempat = $(".error_input_tempat");
+
+  if (input_tempat === "") {
+    error_input_tempat.text("masukkan tempat");
+    is_valid = false;
+  } else {
+    error_input_tempat.text("");
+  }
+
+  var input_tgl_lahir = $(".input_tgl_lahir").val();
+  var error_input_tgl_lahir = $(".error_input_tgl_lahir");
+
+  if (input_tgl_lahir === "") {
+    error_input_tgl_lahir.text("masukkan tanggal");
+    is_valid = false;
+  } else {
+    error_input_tgl_lahir.text("");
+  }
+
+  var input_jk = $(".input_jk").val();
+  var error_input_jk = $(".error_input_jk");
+
+  if (input_jk === "") {
+    error_input_jk.text("pilih jenis kelamin");
+    is_valid = false;
+  } else {
+    error_input_jk.text("");
+  }
+
+  var input_kebangsaan = $(".input_kebangsaan").val();
+  var error_input_kebangsaan = $(".error_input_kebangsaan");
+
+  if (input_kebangsaan === "") {
+    error_input_kebangsaan.text("masukkan kebangsaan");
+    is_valid = false;
+  } else {
+    error_input_kebangsaan.text("");
+  }
+
+  var input_alamat_rumah = $(".input_alamat_rumah").val();
+  var error_input_alamat_rumah = $(".error_input_alamat_rumah");
+
+  if (input_alamat_rumah === "") {
+    error_input_alamat_rumah.text("masukkan alamat rumah");
+    is_valid = false;
+  } else {
+    error_input_alamat_rumah.text("");
+  }
+
+  var input_nomor_regis = $(".input_nomor_regis").val();
+  var error_input_nomor_regis = $(".error_input_nomor_regis");
+
+  if (input_nomor_regis === "") {
+    error_input_nomor_regis.text("masukkan nomor regis");
+    is_valid = false;
+  } else {
+    error_input_nomor_regis.text("");
+  }
+
+  var input_tanda_tangan = $(".input_tanda_tangan").val();
+  var error_input_tanda_tangan = $(".error_input_tanda_tangan");
+
+  if (input_tanda_tangan === "") {
+    error_input_tanda_tangan.text("upload tanda tangan");
+    is_valid = false;
+  } else {
+    error_input_tanda_tangan.text("");
+  }
+
+  if (is_valid == false) {
+    return "";
+  }
+
+  swal(
+    {
+      title: "Pastikan Data Data Telah benar?",
+      // text: "You will not be able to recover this imaginary file !!",
+      type: "warning",
+      showCancelButton: !0,
+      confirmButtonColor: "#7066e0",
+      confirmButtonText: "Ya, Lanjutkan Simpan",
+      closeOnConfirm: !1,
+    },
+    function () {
+      swal("Berhasil !!", "Data Berhasil Di Simpan", "success");
+    }
+  );
+  $(".input_nama_lengkap_asesor").val("");
+  $(".input_inisial_asesor").val("");
+  $(".input_tempat").val("");
+  $(".input_tgl_lahir").val("");
+  $(".input_jk").val("");
+  $(".input_kebangsaan").val("");
+  $(".input_alamat_rumah").val("");
+  $(".input_nomor_regis").val("");
+  $(".input_tanda_tangan").val("");
+});
+
+// dokumensertifikasiasesor_asesor
+$("simpan_dokumensertifikasiasesor_asesor").on("click", function () {
+  var is_valid = true;
+
+  var input_skema = $(".input_skema").val();
+  var input_skema = $(".input_skema");
+
+  if (input_skema === "") {
+    input_skema.text("pilih skema");
+    is_valid = false;
+  } else {
+    input_skema.text("");
+  }
+
+  var input_nomor_regis = $(".input_nomor_regis").val();
+  var error_input_nomor_regis = $(".error_input_nomor_regis");
+
+  if (input_nomor_regis === "") {
+    error_input_nomor_regis.text("masukkan nomor regis");
+    is_valid = false;
+  } else {
+    error_input_nomor_regis.text("");
+  }
+
+  var input_tgl_habis = $(".input_tgl_habis").val();
+  var error_input_tgl_habis = $(".error_input_tgl_habis");
+
+  if (input_tgl_habis === "") {
+    error_input_tgl_habis.text("masukkan tangal habis");
+    is_valid = false;
+  } else {
+    error_input_tgl_habis.text("");
+  }
+
+  var input_tandatangan = $(".input_tandatangan").val();
+  var error_input_tandatangan = $(".error_input_tandatangan");
+
+  if (input_tandatangan === "") {
+    error_input_tandatangan.text("upload tanda tangan");
+    is_valid = false;
+  } else {
+    error_input_tandatangan.text("");
+  }
+  
+  if (is_valid == false) {
+    return "";
+  }
+
+  swal(
+    {
+      title: "Pastikan Data Data Telah benar?",
+      // text: "You will not be able to recover this imaginary file !!",
+      type: "warning",
+      showCancelButton: !0,
+      confirmButtonColor: "#7066e0",
+      confirmButtonText: "Ya, Lanjutkan Simpan",
+      closeOnConfirm: !1,
+    },
+    function () {
+      swal("Berhasil !!", "Data Berhasil Di Simpan", "success");
+    }
+  );
+  $(".input_skema").val('');
+$(".input_nomor_regis").val('');
+$(".input_tgl_habis").val('');
+$(".input_tandatangan").val('');
+});
+$;
